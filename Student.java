4d3a519419dd7499001d3c1db2cbc1b4df9c70e8
nishant-s7 <nishantshinde316@gmail.com>
2022-10-06 15:17:43 +0530

    student

---
+++ Student.java
@@ -0,0 +1,11 @@
+abstract class Student {
+
+    private String name;
+    private String category;
+    private char gender;
+    private int rank;
+    private int rollNumber;
+    private String preferredState;
+    private String email;
+
+}
