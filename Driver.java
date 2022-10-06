4934800398398d31437ef800353f8a356f389d3e
Omkar-Khairnar <omkarkhairnar9441@gmail.com>
2022-10-06 16:54:08 +0530

    Driver.java

--- Driver.java
+++ Driver.java
@@ -2,7 +2,7 @@ public class Driver {
     public static void main (String [] args) {
         
         System.out.println("College Predictor");
-        System.out.println("Hello World");
+        System.out.println("Hello college World");
         
     }
 }
