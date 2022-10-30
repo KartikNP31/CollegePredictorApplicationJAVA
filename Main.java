d27b60f18b2a7f1449295eee3dec4623f8097ae0
KartikNP31 <patelkn1303@gmail.com>
2022-10-31 00:09:46 +0530

    Main updated

--- Main.java
+++ Main.java
@@ -1,28 +1,25 @@
-// public class Driver {
-//     public static void main (String [] args) {
-
-//         System.out.println("College Predictor");
-//         System.out.println("Hello college World");
-
-
-//     }
-// }
 
 import java.sql.*;
-import java.util.*;
+
 
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/JAVAPROJECT", "root",
-                    "ace@mysql325");
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from dummy");
-            while (rs.next()) {
-                System.out.println(rs.getString(1) + "  " + rs.getInt(2) + "  " + rs.getInt(3));
+            Connection connnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/java_proj_college_predictor", "root","#KAR331@tikNP");
+            String name = "damin";
+            String pass= "ace";
+            String email = "daminkhan@gmail.com";
+//            String [] column = {"username","password","e_mail"};
+            Statement statement = connnection.createStatement();
+//            statement.execute("insert into user_details value('"+name+"', '"+pass +"', '"+email+"')");
+
+            ResultSet resultSet = statement.executeQuery("select * from user_details where username='"+name+"' and password='"+pass+"'");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(1) + "  " + resultSet.getString(2) + "  " + resultSet.getString(3));
             }
-            con.close();
+            connnection.close();
         } catch (Exception e) {
             System.out.println(e);
         }
