eda978353fc20400db4c8cd2307c7d2815a50afa
Omkar-Khairnar <omkarkhairnar9441@gmail.com>
2022-10-31 22:35:51 +0530

    "User Updated"

--- User.java
+++ User.java
@@ -1,102 +1,115 @@
 import java.sql.*;
 
+public class User extends Person {
+  private String gender;
+  private String category;
+  // private String examType;
+  private int GeneralRank;
+  private int CategoryRank;
 
-public class User extends Person{
+  // private int rollNumber;
+  // private final String userRole = "user";
 
-    private String gender;
-    private String category;
-    private String examType;
-    private int GeneralRank;
-    private int CategoryRank;
-    private int rollNumber;
-    private final String userRole = "user";
+  User() {}
 
-    User() {
-        
+  public User(String username, String email, String password) {
+    setUsername(username);
+    setEmail(email);
+    setPassword(password);
+  }
+
+  public void setCategory(String category) {
+    this.category = category;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+
+  // public void setExamType(String examType) {
+  //     this.examType = examType;
+  // }
+
+  public void setGeneralRank(int GeneralRank) {
+    this.GeneralRank = GeneralRank;
+  }
+
+  public void setCategoryRank(int categoryRank) {
+    CategoryRank = categoryRank;
+  }
+
+  // public void setRollNumber(int rollNumber) {
+  //     this.rollNumber = rollNumber;
+  // }
+
+  public String getCategory() {
+    return category;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  // public String getExamType() {
+  //     return examType;
+  // }
+
+  public int getGeneralRank() {
+    return GeneralRank;
+  }
+
+  public int getCategoryRank() {
+    return CategoryRank;
+  }
+
+  // public int getRollNumber() {
+  //     return rollNumber;
+  // }
+
+  public void register(
+    String userName,
+    String email,
+    String password,
+    String gender,
+    String category,
+    int generalrank,
+    int categoryrank
+  ) {
+    super.setUsername(userName);
+    super.setEmail(email);
+    super.setPassword(password);
+    setCategory(category);
+    setCategoryRank(categoryrank);
+    setGender(gender);
+    setGeneralRank(generalrank);
+  }
+
+  public boolean Login() {
+    try {
+      Connection connection = DriverManager.getConnection(
+        "jdbc:mysql://localhost:3306/java_proj_college_predictor",
+        "root",
+        "#KAR331@tikNP"
+      );
+      PreparedStatement statement = connection.prepareStatement(
+        "select count(*) from user_details where (username= ? or e_mail= ?) and password = ? "
+      );
+
+      statement.setString(1, getUsername());
+      statement.setString(2, getEmail());
+      statement.setString(3, getPassword());
+
+      ResultSet resultSet = statement.executeQuery();
+
+      if (resultSet.next() && resultSet.getInt(1) == 1) {
+        connection.close();
+        return true;
+      } else {
+        connection.close();
+        return false;
+      }
+    } catch (Exception e) {
+      return false;
     }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public void setExamType(String examType) {
-        this.examType = examType;
-    }
-
-    public void setGeneralRank(int GeneralRank) {
-        this.GeneralRank = GeneralRank;
-    }
-
-    public void setCategoryRank(int categoryRank) {
-        CategoryRank = categoryRank;
-    }
-
-    public void setRollNumber(int rollNumber) {
-        this.rollNumber = rollNumber;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public String getExamType() {
-        return examType;
-    }
-
-    public int getGeneralRank() {
-        return GeneralRank;
-    }
-
-    public int getCategoryRank() {
-        return CategoryRank;
-    }
-
-    public int getRollNumber() {
-        return rollNumber;
-    }
-
-    public void register(String userName, String email,String password) {
-        super.setUsername(userName);
-        super.setEmail(email);
-        super.setPassword(password);
-
-    }
-
-    public boolean Login()
-    {
-        try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/java_proj_college_predictor", "root","#KAR331@tikNP");
-            PreparedStatement statement = connection.prepareStatement("select count(*) from user_details where (username= ? or e_mail= ?) and password = ? ");
-
-            statement.setString(1,getUsername());
-            statement.setString(2,getEmail());
-            statement.setString(3,getPassword());
-
-
-            ResultSet resultSet = statement.executeQuery();
-
-            if(resultSet.next() && resultSet.getInt(1)==1)
-            {
-                connection.close();
-                return true;
-            }else {
-                connection.close();
-                return false;
-            }
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
+  }
 }
-
-
-
