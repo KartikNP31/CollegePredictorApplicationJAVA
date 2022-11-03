93891dce5997cd2a5fe063b325978fd086fe6bef
Omkar-Khairnar <omkarkhairnar9441@gmail.com>
2022-11-04 00:59:05 +0530

    User Updated

--- Admin.java
+++ Admin.java
@@ -1,17 +1,19 @@
- import java.sql.*;
-public class Admin extends Person{
-    
-    Admin()
-    {
-        this(null,null);
+import java.sql.*;
+import java.util.Collections;
+
+public class Admin extends Person {
+
+    Admin() {
+
+        this(null, null);
     }
-    Admin(String username,String password)
-    {
+
+    Admin(String username, String password) {
         setUsername(username);
         setPassword(password);
     }
-    public boolean addNewAdmin(String adminID, String email, String password)
-    {
+
+    public boolean addNewAdmin(String adminID, String email, String password) {
         try {
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/java_proj_college_predictor", "root", "#KAR331@tikNP");
             PreparedStatement statement = connection.prepareStatement("insert into admin_details(adminID,email,Password) values(?,?,?)");
@@ -21,41 +23,36 @@ public class Admin extends Person{
             statement.execute();
             connection.close();
             return true;
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             System.out.println("Above AdminID or e-mail ID is already a administrator.\nPlease Login with your Username/email ID and Password!");
             return false;
         }
     }
 
-    public boolean Login(Connection connection)
-    {
+    public boolean Login(Connection connection) {
         try {
             PreparedStatement statement = connection.prepareStatement("select count(*) from admin_details where (adminID= ? or email= ?) and password = ?");
-            statement.setString(1,getUsername());
-            statement.setString(2,getUsername());
-            statement.setString(3,getPassword());
+            statement.setString(1, getUsername());
+            statement.setString(2, getUsername());
+            statement.setString(3, getPassword());
 
             ResultSet resultSet = statement.executeQuery();
 
-            if(resultSet.next() && resultSet.getInt(1)==1)
-            {
+            if (resultSet.next() && resultSet.getInt(1) == 1) {
 
                 PreparedStatement retriveUser = connection.prepareStatement("select * from admin_details where (adminID= ? or email= ?) and password = ?");
-                retriveUser.setString(1,getUsername());
-                retriveUser.setString(2,getUsername());
-                retriveUser.setString(3,getPassword());
+                retriveUser.setString(1, getUsername());
+                retriveUser.setString(2, getUsername());
+                retriveUser.setString(3, getPassword());
                 ResultSet resultSet1 = retriveUser.executeQuery();
-                while (resultSet1.next())
-                {
+                while (resultSet1.next()) {
                     setUsername(resultSet1.getString("adminID"));
                     setEmail(resultSet1.getString("email"));
                     setPassword(resultSet1.getString("password"));
                 }
                 connection.close();
                 return true;
-            }else {
+            } else {
                 connection.close();
                 return false;
             }
@@ -64,25 +61,21 @@ public class Admin extends Person{
         }
     }
 
-    public void deletecollegeallrounds(String InstituteName){
+    public void deletecollegeallrounds(Connection connection, String InstituteName) {
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/java_proj_college_predictor", "root", "#KAR331@tikNP");
             Statement stmt = connection.createStatement();
 
-            ResultSet rs1 = stmt.executeQuery("DELETE FROM round1 WHERE Institute = '" + InstituteName + "'");
-            ResultSet rs2 = stmt.executeQuery("DELETE FROM round2 WHERE Institute = '" + InstituteName + "'");
-            ResultSet rs3 = stmt.executeQuery("DELETE FROM round3 WHERE Institute = '" + InstituteName + "'");
-            ResultSet rs4 = stmt.executeQuery("DELETE FROM round4 WHERE Institute = '" + InstituteName + "'");
-            ResultSet rs5 = stmt.executeQuery("DELETE FROM round5 WHERE Institute = '" + InstituteName + "'");
-            ResultSet rs6 = stmt.executeQuery("DELETE FROM round6 WHERE Institute = '" + InstituteName + "'");
 
-            System.out.println("Institute "+ InstituteName +" is sucessfully removed");
+            stmt.executeQuery("DELETE FROM round1 WHERE Institute = '" + InstituteName + "'");
+            stmt.executeQuery("DELETE FROM round2 WHERE Institute = '" + InstituteName + "'");
+            stmt.executeQuery("DELETE FROM round3 WHERE Institute = '" + InstituteName + "'");
+            stmt.executeQuery("DELETE FROM round4 WHERE Institute = '" + InstituteName + "'");
+            stmt.executeQuery("DELETE FROM round5 WHERE Institute = '" + InstituteName + "'");
+            stmt.executeQuery("DELETE FROM round6 WHERE Institute = '" + InstituteName + "'");
 
-            connection.close();
+            System.out.println("Institute " + InstituteName + " is sucessfully removed");
 
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
